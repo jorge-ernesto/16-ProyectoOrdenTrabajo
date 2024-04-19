4cc3a7a234d4c33911dfc7060b3f017011f303d4
Jorge Ernesto <jorge.cywdt@gmail.com>
2024-04-19 13:59:06 -0500

    a

--- src/FileCabinet/SuiteScripts/Biomont_SuiteScript/Records/Orden de Trabajo/Bio.SuiteletAPI.OrdenTrabajo.js
+++ src/FileCabinet/SuiteScripts/Biomont_SuiteScript/Records/Orden de Trabajo/Bio.SuiteletAPI.OrdenTrabajo.js
@@ -22,10 +22,12 @@ define(['./lib/Bio.Library.Helper', 'N'],
          * @since 2015.2
          */
         function onRequest(scriptContext) {
+            // Debug
             // scriptContext.response.setHeader('Content-type', 'application/json');
             // scriptContext.response.write(JSON.stringify(scriptContext));
             // return;
 
+            // Debug
             // log.debug('method', scriptContext.request.method);
             // log.debug('parameters', scriptContext.request.parameters);
             // log.debug('body', scriptContext.body);
@@ -61,89 +63,103 @@ define(['./lib/Bio.Library.Helper', 'N'],
                         method: method
                     };
 
-                    if (method == 'getDataFlujoFirmas' && workorderRecord) {
+                    // El control de errores comienza aca para tener acceso a method y response
+                    try {
+                        // Debug
+                        // objHelper.error_log('test err', response);
 
-                        // Obtener datos
-                        let subsidiariaId = workorderRecord.getValue('subsidiary');
-                        let tipoOtId = workorderRecord.getValue('custbody8');
-                        let arrayFlujoFirmas = objHelper.getFlujoFirmas(subsidiariaId, tipoOtId);
+                        if (method == 'getDataFlujoFirmas' && workorderRecord) {
 
-                        // Validar que encontro flujo de firmas
-                        if (Object.keys(arrayFlujoFirmas).length > 0) {
+                            // Obtener datos
+                            let subsidiariaId = workorderRecord.getValue('subsidiary');
+                            let tipoOtId = workorderRecord.getValue('custbody8');
+                            let arrayFlujoFirmas = objHelper.getFlujoFirmas(subsidiariaId, tipoOtId);
 
-                            // Respuesta
-                            response = {
-                                code: '200',
-                                status: 'success',
-                                method: method,
-                                workorderRecord: workorderRecord,
-                                arrayFlujoFirmas: arrayFlujoFirmas
-                            };
+                            // Validar que encontro flujo de firmas
+                            if (Object.keys(arrayFlujoFirmas).length > 0) {
+
+                                // Respuesta
+                                response = {
+                                    code: '200',
+                                    status: 'success',
+                                    method: method,
+                                    workorderRecord: workorderRecord,
+                                    arrayFlujoFirmas: arrayFlujoFirmas
+                                };
+                            }
+                        } else if (method == 'firmar' && workorderRecord) {
+
+                            // Setear datos al record
+                            workorderRecord.setValue(id_campo_usuario_firma, user.id);
+                            workorderRecord.setValue(id_campo_fecha_firma, datetime);
+                            let workorderId = workorderRecord.save();
+                            log.debug('', workorderId);
+
+                            if (workorderId) {
+                                // Obtener url del Record
+                                let urlRecord = url.resolveRecord({
+                                    recordType: 'workorder',
+                                    recordId: workorder_id,
+                                    params: {
+                                        _status: 'PROCESS_SIGNATURE'
+                                    }
+                                })
+
+                                // Respuesta
+                                response = {
+                                    code: '200',
+                                    status: 'success',
+                                    method: method,
+                                    workorderRecord: workorderRecord,
+                                    workorderId: workorderId,
+                                    urlRecord: urlRecord,
+                                    // Otros
+                                    id_campo_usuario_firma: id_campo_usuario_firma,
+                                    id_campo_fecha_firma: id_campo_fecha_firma
+
+                                };
+                            }
+                        } else if (method == 'eliminar_firma' && workorderRecord) {
+
+                            // Setear datos al record
+                            workorderRecord.setValue(id_campo_usuario_firma, '');
+                            workorderRecord.setValue(id_campo_fecha_firma, '');
+                            let workorderId = workorderRecord.save();
+                            log.debug('', workorderId);
+
+                            if (workorderId) {
+                                // Obtener url del Record
+                                let urlRecord = url.resolveRecord({
+                                    recordType: 'workorder',
+                                    recordId: workorder_id,
+                                    params: {
+                                        _status: 'PROCESS_SIGNATURE'
+                                    }
+                                })
+
+                                // Respuesta
+                                response = {
+                                    code: '200',
+                                    status: 'success',
+                                    method: method,
+                                    workorderRecord: workorderRecord,
+                                    workorderId: workorderId,
+                                    urlRecord: urlRecord,
+                                    // Otros
+                                    id_campo_usuario_firma: id_campo_usuario_firma,
+                                    id_campo_fecha_firma: id_campo_fecha_firma
+
+                                };
+                            }
                         }
-                    } else if (method == 'firmar' && workorderRecord) {
-
-                        // Setear datos al record
-                        workorderRecord.setValue(id_campo_usuario_firma, user.id);
-                        workorderRecord.setValue(id_campo_fecha_firma, datetime);
-                        let workorderId = workorderRecord.save();
-                        log.debug('', workorderId);
-
-                        if (workorderId) {
-                            // Obtener url del Record
-                            let urlRecord = url.resolveRecord({
-                                recordType: 'workorder',
-                                recordId: workorder_id,
-                                params: {
-                                    _status: 'PROCESS_SIGNATURE'
-                                }
-                            })
-
-                            // Respuesta
-                            response = {
-                                code: '200',
-                                status: 'success',
-                                method: method,
-                                workorderRecord: workorderRecord,
-                                workorderId: workorderId,
-                                urlRecord: urlRecord,
-                                // Otros
-                                id_campo_usuario_firma: id_campo_usuario_firma,
-                                id_campo_fecha_firma: id_campo_fecha_firma
-
-                            };
-                        }
-                    } else if (method == 'eliminar_firma' && workorderRecord) {
-
-                        // Setear datos al record
-                        workorderRecord.setValue(id_campo_usuario_firma, '');
-                        workorderRecord.setValue(id_campo_fecha_firma, '');
-                        let workorderId = workorderRecord.save();
-                        log.debug('', workorderId);
-
-                        if (workorderId) {
-                            // Obtener url del Record
-                            let urlRecord = url.resolveRecord({
-                                recordType: 'workorder',
-                                recordId: workorder_id,
-                                params: {
-                                    _status: 'PROCESS_SIGNATURE'
-                                }
-                            })
-
-                            // Respuesta
-                            response = {
-                                code: '200',
-                                status: 'success',
-                                method: method,
-                                workorderRecord: workorderRecord,
-                                workorderId: workorderId,
-                                urlRecord: urlRecord,
-                                // Otros
-                                id_campo_usuario_firma: id_campo_usuario_firma,
-                                id_campo_fecha_firma: id_campo_fecha_firma
-
-                            };
-                        }
+                    } catch (err) {
+                        // Respuesta
+                        response = {
+                            code: '400',
+                            status: 'error',
+                            method: method,
+                            err: err
+                        };
                     }
 
                     // Respuesta
