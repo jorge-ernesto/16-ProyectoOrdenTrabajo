719db1d7ae8657318a8d2432015d0c32c6a3733a
Ernesto <jorge.cywdt@gmail.com>
2024-05-20 14:29:04 -0500

    Cambios realizados por Victor Robles

---
+++ src/FileCabinet/SuiteScripts/Biomont_SuiteScript/Records/Orden de Trabajo/Bio.Suitelet.OrdenTrabajo.DescargarArchivo.js
@@ -0,0 +1,30 @@
+// Notas del archivo:
+// - Secuencia de comando:
+//      - Biomont SL Orden Trabajo Descargar Archivo (customscript_bio_sl_orden_trabajo_des_arc)
+
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['./lib/Bio.Library.Helper', 'N'],
+
+    function (objHelper, N) {
+
+        const { log } = N;
+
+        /******************/
+
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        function onRequest(scriptContext) {
+
+        }
+
+        return { onRequest }
+
+    });
---
+++ src/FileCabinet/SuiteScripts/Biomont_SuiteScript/Records/Orden de Trabajo/Validacion/Bio.ClientScript.ValidacionOrdenTrabajo.js
@@ -0,0 +1,60 @@
+// Notas del archivo:
+// - Secuencia de comando:
+//      - Biomont CS Validacion Orden Trabajo (customscript_bio_cs_validacion_orden_trabajo)
+// - Registro:
+//      - Orden de Trabajo (workorder)
+
+// Validación como la usa LatamReady:
+// - ClientScript           : No se ejecuta en modo ver. Solo se ejecuta en modo crear, copiar o editar.
+// - En modo crear o editar : Validamos por el formulario.
+// - En modo ver            : Validamos por el pais de la subsidiaria.
+
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N'],
+
+    function (N) {
+
+        function pageInit(scriptContext) {
+
+            console.log("Script Validación de Enrutamiento en ejecución!")
+        }
+
+        function saveRecord(scriptContext) {
+
+            var currentRecord = scriptContext.currentRecord;
+            var isWIP = currentRecord.getValue({ fieldId: "iswip" });
+
+            if (isWIP) {
+                var bomId = currentRecord.getValue({ fieldId: "billofmaterials" });
+                var bomRevision = currentRecord.getValue({ fieldId: "billofmaterialsrevision" });
+                var routingId = currentRecord.getValue({ fieldId: "manufacturingrouting" });
+
+                if (!bomId) {
+                    alert("Debes completar el campo Lista de Materiales.");
+                    return false; // Cancelar el guardado
+                }
+
+                if (!bomRevision) {
+                    alert("Debes completar el campo Revisión de Lista de Materiales.");
+                    return false; // Cancelar el guardado
+                }
+
+                if (!routingId) {
+                    alert("Debes completar el campo Enrutamiento de Fabricación.");
+                    return false; // Cancelar el guardado
+                }
+            }
+
+            return true; // Permitir el guardado
+        }
+
+        return {
+            pageInit: pageInit,
+            saveRecord: saveRecord
+        };
+
+    });
